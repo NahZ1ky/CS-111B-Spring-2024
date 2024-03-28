c3294f826c8b35d320ee320b7311396f7dc0ceae
Ziky Zhang <lee4our@gmail.com>
2024-03-28 00:19:20 -0700

    Simplify Tester

--- PA/Extra/TemperatureTestZver.java
+++ PA/Extra/TemperatureTestZver.java
@@ -15,10 +15,8 @@ public class TemperatureTestZver
 	TemperatureZver t1 = new TemperatureZver();
    	System.out.println("Results of default constructor:");
 	System.out.println("Verify 0 degrees C.");
-	System.out.println();
 	t1.writeOutput();
 	System.out.println();
-	System.out.println("Hit 'y' to continue.");
 	System.out.println();
 
 	System.out.println("Test case 2: NOT IMPLEMENTED.");
@@ -26,123 +24,88 @@ public class TemperatureTestZver
 	System.out.println("Test case 3: constructor with just"
 		+ " temperature.");
 	TemperatureZver t2 = new TemperatureZver(20.5);
- 	System.out.println();
  	System.out.println("Verify 20.5 degrees C.");
- 	System.out.println();
  	t2.writeOutput();
  	System.out.println();
- 	System.out.println("Hit 'y' to continue.");
 	System.out.println();
 
    	System.out.println("Test case 4: constructor with just units,"
 		+ " F.");
- 	System.out.println();
  	System.out.println("Verify 0 degrees F.");
- 	System.out.println();
  	TemperatureZver t3 = new TemperatureZver('F');
  	t3.writeOutput();
  	System.out.println();
- 	System.out.println("Hit 'y' to continue.");
  	System.out.println();
 
 	System.out.println("Test case 5: constructor with just units,"
 				+ " C.");
-    System.out.println();
  	System.out.println("Verify 0 degrees C.");
- 	System.out.println();
  	TemperatureZver t4 = new TemperatureZver('C');
  	t4.writeOutput();
  	System.out.println();
- 	System.out.println("Hit 'y' to continue.");
  	System.out.println();
 
 	System.out.println("Test case 6: constructor with"
 			+ " temperature and units.");
- 	System.out.println();
  	System.out.println("Verify -51.2 degrees F.");
- 	System.out.println();
  	TemperatureZver t5 = new TemperatureZver(-51.2, 'F');
  	t5.writeOutput();
  	System.out.println();
- 	System.out.println("Hit 'y' to continue.");
  	System.out.println();
 
 	System.out.println("Test case 7: write degrees C, "
 			+ "original in F.");
-	System.out.println();
 	System.out.println("Verify -46.2 degrees C.");
-	System.out.println();
 	t5.writeC();
 	System.out.println();
-	System.out.println("Hit 'y' to continue.");
  	System.out.println();
 
 	System.out.println("Test case 8: write degrees C, "
 			+ "original in C.");
-	System.out.println();
 	System.out.println("Verify 0 degrees C.");
-	System.out.println();
 	t4.writeC();
 	System.out.println();
-	System.out.println("Hit 'y' to continue.");
  	System.out.println();
 
 	System.out.println("Test case 9: write degrees F, "
 			+ "original in C.");
-	System.out.println();
 	System.out.println("Verify 32 degrees F");
-	System.out.println();
 	t4.writeF();
 	System.out.println();
-	System.out.println("Hit 'y' to continue.");
  	System.out.println();
 
   	System.out.println("Test case 10: write degrees F, "
 			+ " original in F.");
-	System.out.println();
 	System.out.println("Verify -51.2 degrees F");
-	System.out.println();
 	t5.writeF();
 	System.out.println();
-	System.out.println("Hit 'y' to continue.");
  	System.out.println();
 
 	System.out.println("Test case 11: getF method with "
 			+ "original temperature in F.");
-	System.out.println();
 	System.out.println("Verify -51.2");
-	System.out.println();
 	System.out.println(t5.getF());
-	System.out.println();
-	System.out.println("Hit 'y' to continue.");
  	System.out.println();
 
 	System.out.println("Test case 12: getF method with original"
 			+ " temperature in C.");
 	System.out.println("Verify 32");
-	System.out.println();
 	System.out.println(t4.getF());
 	System.out.println();
-	System.out.println("Hit 'y' to continue.");
  	System.out.println();
 
 	System.out.println("Test case 13: getC method with "
 			+ "original temperature in F.");
 	System.out.println("Verify -46.2");
-	System.out.println();
 	System.out.println(t5.getC());
 	System.out.println();
-	System.out.println("Hit 'y' to continue.");
 	System.out.println();
 
-     	System.out.println();
+    System.out.println();
 	System.out.println("Test case 14: getC method with "
 			+ "original temperature in C.");
 	System.out.println("Verify 0");
-	System.out.println();
 	System.out.println(t4.getC());
-	System.out.println();
-	System.out.println("Hit 'y' to continue.");
 	System.out.println();
 
 	System.out.println();
@@ -152,10 +115,8 @@ public class TemperatureTestZver
 	t5.writeOutput();
 	t5.set(72.8, 'C');
 	System.out.println("After set: verify 72.8 degrees C.");
-	System.out.println();
 	t5.writeOutput();
 	System.out.println();
-	System.out.println("Hit 'y' to continue.");
  	System.out.println();
 
 	System.out.println("Test case 16: set method for just "
@@ -164,10 +125,8 @@ public class TemperatureTestZver
 	t5.writeOutput();
 	t5.set(100);
 	System.out.println("After set: verify 100 degrees C.");
-	System.out.println();
 	t5.writeOutput();
 	System.out.println();
-	System.out.println("Hit 'y' to continue.");
 	System.out.println();
 
 	System.out.println("Test case 17: set method for just units.");
@@ -175,10 +134,8 @@ public class TemperatureTestZver
 	t5.writeOutput();
 	t5.set('F');
 	System.out.println("After set: verify 100 degrees F.");
-	System.out.println();
 	t5.writeOutput();
 	System.out.println();
-	System.out.println("Hit 'y' to continue.");
 	System.out.println();
 
 	System.out.println("Test case 18 (at last): toString method");
